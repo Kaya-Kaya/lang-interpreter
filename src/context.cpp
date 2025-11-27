ec079a7b8dcee26ef465b261a4ac029816135af4
Nikita Gorshkov <gorshkovnikita@gmail.com>
2025-11-27 01:58:50 -0500

    undefined variable exception

--- src/context.cpp
+++ src/context.cpp
@@ -1,4 +1,5 @@
 #include "expression/expression.hpp"
+#include "exception/runtime_exception.hpp"
 #include "context.hpp"
 #include "value.hpp"
 
@@ -10,7 +11,7 @@ TypedValue Context::lookup(const std::string& name) const {
     try {
         return _variables.at(name);
     } catch (std::out_of_range& e) {
-        // throw error
+        throw UndefinedVariableException(name);
     }
 }
 
