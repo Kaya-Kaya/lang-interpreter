8957be6e4fa9a9dbb77f12cb707381824f6f74ff
Nikita Gorshkov <gorshkovnikita@gmail.com>
2025-11-27 03:21:36 -0500

    context tests

---
+++ tests/test_context.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+
+#include "context.hpp"
+#include "exception/runtime_exception.hpp"
+#include "type/number.hpp"
+#include "value.hpp"
+
+TEST(ContextTest, BasicAssignAndLookup) {
+    Context ctx;
+    TypedValue val(NumberType(100), Type::NUMBER);
+    
+    ctx.assign("myVar", val);
+    TypedValue result = ctx.lookup("myVar");
+    
+    EXPECT_EQ(result.getType(), Type::NUMBER);
+    EXPECT_DOUBLE_EQ(std::get<NumberType>(result.getValue()).getValue(), 100.0);
+}
+
+TEST(ContextTest, OverwriteVariable) {
+    Context ctx;
+    TypedValue val1(NumberType(1), Type::NUMBER);
+    TypedValue val2(NumberType(2), Type::NUMBER);
+    
+    ctx.assign("x", val1);
+    EXPECT_DOUBLE_EQ(std::get<NumberType>(ctx.lookup("x").getValue()).getValue(), 1.0);
+    
+    ctx.assign("x", val2);
+    EXPECT_DOUBLE_EQ(std::get<NumberType>(ctx.lookup("x").getValue()).getValue(), 2.0);
+}
+
+TEST(ContextTest, LookupUndefinedThrows) {
+    Context ctx;
+    EXPECT_THROW(ctx.lookup("undefined"), UndefinedVariableException);
+}
