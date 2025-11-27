ab0fddc1ca0dc36d706b54a93ed727713b2df620
Kaya Tacer <ktacer@proton.me>
2025-11-26 22:40:50 -0800

    remove virtual functions from BaseType

--- include/type/type.hpp
+++ include/type/type.hpp
@@ -10,23 +10,6 @@ private:
 
 public:
     BaseType(T value) : _value(value) {}
-    virtual ~BaseType() = default;
-
-    virtual TypedValue logicalNot() const = 0;
-    virtual TypedValue logicalOr(const TypedValue other) const = 0;
-    virtual TypedValue logicalAnd(const TypedValue other) const = 0;
-
-    virtual TypedValue add(const TypedValue other) const = 0;
-    virtual TypedValue subtract(const TypedValue other) const = 0;
-    virtual TypedValue multiply(const TypedValue other) const = 0;
-    virtual TypedValue divide(const TypedValue other) const = 0;
-
-    virtual TypedValue isEqual(const TypedValue other) const = 0;
-    virtual TypedValue isNotEqual(const TypedValue other) const = 0;
-    virtual TypedValue isLess(const TypedValue other) const = 0;
-    virtual TypedValue isGreater(const TypedValue other) const = 0;
-    virtual TypedValue isLessOrEqual(const TypedValue other) const = 0;
-    virtual TypedValue isGreaterOrEqual(const TypedValue other) const = 0;
 
     T getValue() const {
         return _value;
